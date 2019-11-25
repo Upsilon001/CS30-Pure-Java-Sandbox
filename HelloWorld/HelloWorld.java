2c01b53b181f8032e0527252273faa82096158fe
MercersKitchen <Mark.Mercer@epsb.ca>
2019-11-25 11:24:36 -0700

    Update HelloWorld.java

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
   public static void main (String[] args)
   {
-    System.out.println(" ");
-    System.out.println("Hello World!");
-    System.out.println(" ");
+    //System.out.println(" ");
+    System.out.println("/nHello World!/n");
+    //System.out.println(" ");
   }
 }
