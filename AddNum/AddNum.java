263a3141dc24deccbd33f4b95a5eb88b5c122845
MercersKitchen <Mark.Mercer@epsb.ca>
2019-11-26 10:50:24 -0700

    Update

---
+++ AddNum/AddNum.java
@@ -0,0 +1,8 @@
+public class AddNum {
+  public static void main(String args[]) {
+    int num1 = 1;
+    int num2 = 2;
+
+    System.out.println("\nSum of two numbers is: \nNumber 1 is " + num1 + " Number 2 is " + num2);
+  }
+}
