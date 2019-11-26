2560c22f3f859aafd2fd36e26f29c0c784161dc6
MercersKitchen <Mark.Mercer@epsb.ca>
2019-11-26 15:10:20 -0700

    Update

--- AddNum/ReadMe.txt
+++
@@ -1,3 +0,0 @@
-
-Build this
-Test different numbers
--- Previous/HelloWorld/HelloWorld.java
+++ Previous/HelloWorld/HelloWorld.java
@@ -1,8 +1,11 @@
+//Reviews requirements for Java Compiler to read program code,
+//    finding first line of the code
+//  Compare this to Processing-Java
 public class HelloWorld {
   public static void main (String[] args)
   {
     //System.out.println(" ");
     System.out.println("\nHello World!\n");
     //System.out.println(" ");
-  }
-}
+  } //End main()
+} //End class HelloWorld
