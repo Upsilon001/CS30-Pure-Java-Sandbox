f7838e569cabf7d8874816f04de58b8ea41ea141
MercersKitchen <Mark.Mercer@epsb.ca>
2019-11-26 11:09:14 -0700

    Update

--- AddNum/AddNum.java
+++ AddNum/AddNum.java
@@ -7,11 +7,8 @@ public class AddNum {
   public static void main(String args[]) {
     AddNum obj = new AddNum();
 
-    int num1 = 1;
-    int num2 = 2;
+    int ans = obj.addNum(1, 2);
 
-    int ans = obj.addNum(num1, num2);
-
-    System.out.println("\nSum of two numbers is: " + ans + "\n  Number 1 is " + num1 + "\n  Number 2 is " + num2 + "\n");
+    System.out.println("\nSum of two numbers is: " + ans + "\n");
   }
 }
