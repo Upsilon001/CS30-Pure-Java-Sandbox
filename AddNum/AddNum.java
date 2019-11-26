388dc56ce5f8b95e58c9044d1d1935afa7a0c538
MercersKitchen <Mark.Mercer@epsb.ca>
2019-11-26 11:13:31 -0700

    Update

--- AddNum/AddNum.java
+++ AddNum/AddNum.java
@@ -4,11 +4,17 @@ public class AddNum {
     return numA + numB;
   }
 
+  public float divideNum (int numA, int numB) {
+    return numA / numB;
+  }
+
   public static void main(String args[]) {
     AddNum obj = new AddNum();
 
-    int ans = obj.addNum(1, 2);
+    int ansSum = obj.addNum(1, 2);
+    float ansDiv = obj.divideNum(4, 2);
 
-    System.out.println("\nSum of two numbers is: " + ans + "\n");
+    System.out.println("\nSum of two numbers is: " + ansSum + "\n");
+    System.out.println("\nDivision of two numbers is: " + ansDiv + "\n");
   }
 }
