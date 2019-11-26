0cd9303d744a7e54e7004f25b0550e3667995929
MercersKitchen <Mark.Mercer@epsb.ca>
2019-11-26 11:48:43 -0700

    update

--- AddNum/AddNum.java
+++ AddNum/AddNum.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class AddNum {
 
   public int addNum (int numA, int numB) {
@@ -8,12 +10,18 @@ public class AddNum {
     return numA / numB;
   }
 
-  public static void main(String args[]) {
+  public static void main(String args[])
+  throws IOException
+  {
     AddNum obj = new AddNum();
-
     int ansSum = obj.addNum(1, 2);
-    float ansDiv = obj.divideNum(4, 2);
-
+    float ansDiv;
+    try {
+      ansDiv = obj.divideNum(4, 0);
+    } catch(Exception e) {
+      System.out.println("Please do not divide by zero.");
+      ansDiv = 108; //infinity
+    }
     System.out.println("\nSum of two numbers is: " + ansSum + "\n");
     System.out.println("\nDivision of two numbers is: " + ansDiv + "\n");
   }
