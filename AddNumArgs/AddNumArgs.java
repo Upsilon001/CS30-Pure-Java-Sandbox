14ba942fcfec099673bea49abee63683ae1c6cb8
MercersKitchen <Mark.Mercer@epsb.ca>
2019-11-29 15:01:50 -0700

    Update

--- AddNumArgs/AddNumArgs.java
+++ AddNumArgs/AddNumArgs.java
@@ -8,6 +8,8 @@ public class AddNumArgs {
   static double[] numDouble = new double[2];
   static float[] numFloat = new float[2];
   static String[] input = new String[2];
+  static Boolean keyDouble = false;
+  static Boolean keyLong = false;
 
   public long addNum (long numA, long numB) {
     return numA + numB;
@@ -42,16 +44,17 @@ public class AddNumArgs {
     try {
       input[0] = args[0];
       input[1] = args[1];
-      System.out.println("Congrats ... I am using your ARGS.");
+      System.out.println("\nCongrats ... I am using your ARGS.");
     }
     catch (ArrayIndexOutOfBoundsException e3) {
-      System.out.println("A serious error has occured in the algorithm.");
+      System.out.println("\nA serious error has occured in the algorithm.");
       System.exit(0);
     }
     try {
       System.out.println("Let's see if you typed whole numbers ...");
       numLong[0] = Long.parseLong(input[0]);
       numLong[1] = Long.parseLong(input[1]);
+      keyLong = true;
       //numInt[0] = Integer.parseInt(input[0]);
     }
     catch (NumberFormatException e4) {
@@ -59,6 +62,7 @@ public class AddNumArgs {
         System.out.println("Let's see if you typed decimals ...");
         numDouble[0] = Double.parseDouble(input[0]);
         numDouble[1] = Double.parseDouble(input[1]);
+        keyDouble = true;
         //Float.parseFloat(input[0]);
       }
       catch (NumberFormatException e5) {
@@ -72,8 +76,18 @@ public class AddNumArgs {
     //Need to check if args[2] is empty, this is good
     //Need to parse from strings to ... this becomes an algorithm
     //
-    long ansSum = obj.addNum(numLong[0], numLong[1]);
-    double ansDiv = obj.divideNum(numDouble[0], numDouble[1]);;
+    if (keyLong == true) {
+      long ansSum = obj.addNum(numLong[0], numLong[1]);
+      System.out.println("\nYour whole numbers are: " + numLong[0] + " & " + numLong[1]);
+      System.out.println("\nSum of two numbers is: " + ansSum);
+    }
+    if (keyDouble == true) {
+      double ansDiv = obj.divideNum(numDouble[0], numDouble[1]);
+      System.out.println("\nYour decimal numbers are: " + numDouble[0] + " & " + numDouble[1]);
+      System.out.println("\nDivision of two numbers is: " + ansDiv);
+      System.out.printf("%.2f", ansDiv);
+    }
+    System.out.println("\n");
     /*
     try { //forcing division by zero, java.io has smrt response
       ansDiv = obj.divideNum(args[0], args[1]);
@@ -85,11 +99,5 @@ public class AddNumArgs {
       System.out.printf("%.2f", ansDiv);
     }
     */
-    System.out.println("\nYour whole numbers are: " + numLong[0] + " & " + numLong[1]);
-    System.out.println("\nYour decimal numbers are: " + numDouble[0] + " & " + numDouble[1]);
-    System.out.println("\nSum of two numbers is: " + ansSum);
-    System.out.println("\nDivision of two numbers is: " + ansDiv);
-    System.out.printf("%.2f", ansDiv);
-    System.out.println("\n");
   }
 }
