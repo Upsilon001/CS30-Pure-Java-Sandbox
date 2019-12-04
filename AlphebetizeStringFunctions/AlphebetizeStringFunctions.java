1627751347ccc8129aed622c2a18c35e78619b8a
MercersKitchen <Mark.Mercer@epsb.ca>
2019-12-04 10:59:14 -0700

    Update

--- AlphebetizeStringFunctions/AlphebetizeStringFunctions.java
+++ AlphebetizeStringFunctions/AlphebetizeStringFunctions.java
@@ -1,10 +1,12 @@
 public class AlphebetizeStringFunctions {
 
   //Global Variables
-  public static String alphabet = "The quick brown fox jumps over the lazy dog.";
+  public static String alphabet = "  The quick brown fox jumps over the lazy dog.";
 
   public static void main (String[] args)
   {
     System.out.println(alphabet);
+    String memAlphabet = alphabet.trim();
+    System.out.println(memAlphabet);
   }
 }
