06a333be3781c9b04d5b5d3e5394f9f3859d0466
MercersKitchen <Mark.Mercer@epsb.ca>
2019-12-05 14:31:53 -0700

    UPdate

---
+++ Test/AlphebetizeStringFunctions/AlphebetizeStringFunctions.java
@@ -0,0 +1,58 @@
+public class AlphebetizeStringFunctions {
+
+  //Global Variables
+  public static String alphabet = "  The  quick  brown fox jumps  over the  lazy dog.";
+
+  public static void main (String[] args)
+  {
+    System.out.println(alphabet);
+    //Modify variable to delete extra spaces.
+    String memAlphabet = alphabet.trim();
+    System.out.println(memAlphabet);
+    //Modify for lowercase
+    String lowerCaseMemAlphabet = memAlphabet.toLowerCase();
+    System.out.println(lowerCaseMemAlphabet);
+    //Find the end of the sentence, limited to periods
+    int periodPositionI = lowerCaseMemAlphabet.indexOf(".");
+    System.out.println(periodPositionI);
+    String periodPositionS = lowerCaseMemAlphabet.substring(periodPositionI, periodPositionI+1);
+    Boolean periodPositionB = periodPositionS.equals(".");
+    System.out.println(periodPositionB);
+    String noPeriodlowerCaseMemAlphabet = lowerCaseMemAlphabet.substring(0, periodPositionI);
+    System.out.println(noPeriodlowerCaseMemAlphabet);
+    //Create Substrings for all words
+    int word1space = noPeriodlowerCaseMemAlphabet.indexOf(" ");
+    System.out.println(word1space);
+    String word1 = noPeriodlowerCaseMemAlphabet.substring(0, word1space);
+    System.out.println(word1);
+    System.out.println( word1.length() );
+    Boolean word1True;
+    if ( word1space == word1.length() ) {
+      word1True = true;
+    } else {
+      word1True = false;
+    }
+    System.out.println(word1True);
+    String remainSentence = noPeriodlowerCaseMemAlphabet.substring(word1space, periodPositionI);
+    System.out.println(remainSentence);
+    //Second Word
+    System.out.println("\nSecond Word\n");
+    remainSentence = remainSentence.trim();
+    int word2space = remainSentence.indexOf(" ");
+    System.out.println(word2space);
+    String word2 = remainSentence.substring(0, word2space);
+    System.out.println(word2);
+    //Third Word
+    periodPositionI = periodPositionI - word2space;
+    remainSentence = remainSentence.substring(word2space, periodPositionI);
+    remainSentence = remainSentence.trim();
+    int word3space = remainSentence.indexOf(" ");
+    String word3 = remainSentence.substring(0, word3space);
+    //
+    System.out.println("The third word is " + word3);
+    // Fourth Word
+
+    //What happens if 2 spaces is the default for editting the separation between words
+    //Which functions and which variables need to be repeated to capture the rest of the words
+  }
+}
